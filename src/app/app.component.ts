cec89e4c57bad2babb52696c4a963927841c9062
Bility1 <lanfiab13.mk@gmail.com>
2023-08-21 15:16:53 +0200

    Les variables référencées dans le template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,9 +13,9 @@ export class AppComponent implements OnInit {
   pokemonList:Pokemon[] = POKEMONS;
   ngOnInit() {
     console.table(this.pokemonList);
-    this.selectPokemon(this.pokemonList[0]);
   }
-  selectPokemon(pokemon: Pokemon){
-    console.log(`Vous avez cliqué sur le pokémon ${pokemon.name}`);
+  selectPokemon(event: MouseEvent){
+    const index: number=+(event.target as HTMLInputElement).value;
+    console.log(`Vous avez cliqué sur le pokémon ${this.pokemonList[index].name}`);
   }
 }
