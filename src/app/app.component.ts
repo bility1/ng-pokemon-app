4b0b7678a2955158c9074d5af8e471e8af0b4f29
Bility1 <lanfiab13.mk@gmail.com>
2023-08-22 15:23:34 +0200

    template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,11 +11,18 @@ import { Pokemon } from './pokemon';
 //OnInit est une interface
 export class AppComponent implements OnInit {
   pokemonList:Pokemon[] = POKEMONS;
+  pokemonSelected: Pokemon|undefined;
   ngOnInit() {
     console.table(this.pokemonList);
   }
-  selectPokemon(event: MouseEvent){
-    const index: number=+(event.target as HTMLInputElement).value;
-    console.log(`Vous avez cliqué sur le pokémon ${this.pokemonList[index].name}`);
+  selectPokemon(pokemonId: string){
+    const pokemon: Pokemon|undefined = this.pokemonList.find(pokemon =>pokemon.id == +pokemonId);
+    if(pokemon){
+      console.log(`Vous avez cliqué sur le pokémon ${pokemon.name}`);
+      this.pokemonSelected = pokemon;
+    }else{
+      console.log(`Vous avez cliqué sur un pokémon qui n'existe pas.`);
+      this.pokemonSelected = pokemon;
+    }
   }
 }
