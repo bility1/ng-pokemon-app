1e3a7d8a059187a5447c9d4eb3171eb8e76d30fa
Bility1 <lanfiab13.mk@gmail.com>
2023-08-31 11:09:15 +0200

    details d'un pokemon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
-import { Component ,OnInit} from '@angular/core';
-import { POKEMONS } from './mock-pokemon-list';
-import { Pokemon } from './pokemon';
+import { Component } from '@angular/core';
+
 // contruire un composant 
 @Component({
   selector: 'app-root',
@@ -9,20 +8,6 @@ import { Pokemon } from './pokemon';
       
 })
 //OnInit est une interface
-export class AppComponent implements OnInit {
-  pokemonList:Pokemon[] = POKEMONS;
-  pokemonSelected: Pokemon|undefined;
-  ngOnInit() {
-    console.table(this.pokemonList);
-  }
-  selectPokemon(pokemonId: string){
-    const pokemon: Pokemon|undefined = this.pokemonList.find(pokemon =>pokemon.id == +pokemonId);
-    if(pokemon){
-      console.log(`Vous avez cliqué sur le pokémon ${pokemon.name}`);
-      this.pokemonSelected = pokemon;
-    }else{
-      console.log(`Vous avez cliqué sur un pokémon qui n'existe pas.`);
-      this.pokemonSelected = pokemon;
-    }
-  }
+export class AppComponent {
+ 
 }
--- src/app/detail-pokemon/detail-pokemon.component.ts
+++ src/app/detail-pokemon/detail-pokemon.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Route, Router } from '@angular/router';
+import { POKEMONS } from '../mock-pokemon-list';
+import { Pokemon } from '../pokemon';
 
 @Component({
   selector: 'app-detail-pokemon',
@@ -6,6 +9,17 @@ import { Component } from '@angular/core';
   styles: [
   ]
 })
-export class DetailPokemonComponent {
+export class DetailPokemonComponent implements OnInit {
+  pokemonList : Pokemon[];
+  pokemon : Pokemon|undefined;
+  constructor(private route : ActivatedRoute) {
 
+  }
+  ngOnInit() {
+    this.pokemonList = POKEMONS;
+    const pokemonId : string|null= this.route.snapshot.paramMap.get('id');
+    if(pokemonId){
+      this.pokemon = this.pokemonList.find(pokemon => pokemon.id == +pokemonId)
+   } 
+}
 }
