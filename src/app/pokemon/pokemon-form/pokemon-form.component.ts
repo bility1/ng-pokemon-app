269c2b6fdddb4da2698d8bce2564d27d89995930
Bility1 <lanfiab13.mk@gmail.com>
2024-11-07 14:48:25 +0100

    update

--- src/app/pokemon/pokemon-form/pokemon-form.component.ts
+++ src/app/pokemon/pokemon-form/pokemon-form.component.ts
@@ -11,6 +11,7 @@ import { Router } from '@angular/router';
 export class PokemonFormComponent implements OnInit {
   @Input() pokemon: Pokemon;
   types: string[];
+  isAddForm: boolean;
 
   constructor(
     private pokemonService: PokemonService,
@@ -19,6 +20,7 @@ export class PokemonFormComponent implements OnInit {
   ngOnInit(){
     //pokemonTypeList
     this.types =this.pokemonService.getPokemonTypeList();
+    this.isAddForm = this.router.url.includes('add');
 
   }
 hasType(type :string): boolean{
@@ -37,11 +39,21 @@ if(isChecked) {
 }
 
 onSubmit(){
-  this.pokemonService.updatePokemon(this.pokemon)
+  if(this.isAddForm){
+    this.pokemonService.addPokemon(this.pokemon).subscribe((
+      pokemon: Pokemon
+    ) =>
+      this.router.navigate(['/pokemon', pokemon.id])
+      
+  ); 
+  }else{
+    this.pokemonService.updatePokemon(this.pokemon)
   .subscribe(() =>
         this.router.navigate(['/pokemon', this.pokemon.id])
       
     ); 
+  }
+  
  // this.router.navigate(['/pokemon', this.pokemon.id]);
 }
 
