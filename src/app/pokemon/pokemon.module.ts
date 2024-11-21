49b341fd0bb50ec71445f4d2a3fbf47321220836
Bility1 <lanfiab13.mk@gmail.com>
2024-11-21 10:57:43 +0100

    ajouter une icone de chargement la librairie RXJS

--- src/app/pokemon/pokemon.module.ts
+++ src/app/pokemon/pokemon.module.ts
@@ -11,6 +11,7 @@ import { EditPokemonComponent } from './edit-pokemon/edit-pokemon.component';
 import { AddPokemonComponent } from './add-pokemon/add-pokemon.component';
 import { PokemonFormComponent } from './pokemon-form/pokemon-form.component';
 import { SearchPokemonComponent } from './search-pokemon/search-pokemon.component';
+import { LoaderComponent } from './loader/loader.component';
 
 
 const pokemonRoutes: Routes = [
@@ -32,6 +33,7 @@ const pokemonRoutes: Routes = [
     AddPokemonComponent,
    PokemonFormComponent,
     SearchPokemonComponent,
+    LoaderComponent,
   
   ],
   imports: [
